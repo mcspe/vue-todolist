8d113f9059270029e325a064f5b6bb47a86a3475
mcspe <mcspezzaferro@gmail.com>
2023-04-16 14:45:58 +0200

    [add] dinamic list from data

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@ const {createApp} = Vue;
 createApp({
   data(){
     return {
-      
+      newTask: '',
+      tasks: [
+        {
+          text: 'creare cartella repo',
+          done: false
+        },
+        {
+          text: 'creare file README',
+          done: false
+        },
+        {
+          text: 'inizializzare GitHub',
+          done: false
+        }
+      ]
     }
   },
   methods:{
