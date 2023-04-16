4bebd90dfd072f630e48210804dea504296880ba
mcspe <mcspezzaferro@gmail.com>
2023-04-16 15:06:47 +0200

    [add] toggle done task and style change

--- js/script.js
+++ js/script.js
@@ -17,11 +17,21 @@ createApp({
           text: 'inizializzare GitHub',
           done: false
         }
-      ]
+      ],
+      taskDoneStyle: 'text-bg-danger'
     }
   },
   methods:{
-    
+    toggleDone(index){
+      if (!(this.tasks[index].done)){
+        this.tasks[index].done = !this.tasks[index].done;
+        console.log(this.tasks[index].text, 'vero');
+      } 
+      else {
+        this.tasks[index].done = !this.tasks[index].done;
+        console.log(this.tasks[index].text, 'falso');
+      }
+    }
   },
   mounted(){
     
